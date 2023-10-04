4df4f2914ccbbce7ee0fc61cf5b2ae715edadcd7
Mikhail Verbitskii <vemant@yandex.ru>
2023-10-04 14:39:51 +0300

    Main 4.6.1_7 fixed DashboardPage by instruction'

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -16,11 +16,11 @@ public class DataHelper {
     }
 
     public static CardInfo getFirstCardInfo() {
-        return new CardInfo("5559 0000 0000 0001", "92df3f1c-a033-48e6-8398-206f6b1f56c8");
+        return new CardInfo("5559 0000 0000 0001", "92df3f1c-a033-48e6-8390-206f6b1f56c0");
     }
 
     public static CardInfo getSecondCardInfo() {
-        return new CardInfo("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-89f7a839391d");
+        return new CardInfo("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
     }
 
     public static int generateValidAmount(int balance) {
--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -2,6 +2,7 @@ package ru.netology.page;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import lombok.val;
 import ru.netology.data.DataHelper;
 
 import static com.codeborne.selenide.Condition.attribute;
@@ -9,22 +10,38 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 
 
 public class DashboardPage {
+    private ElementsCollection cards = $$(".list__item div");
     private final String balanceStart = "баланс: ";
-    private final String balanceFinish = " p.";
+    private final String balanceFinish = " р.";
     private final SelenideElement heading = $("[data-test-id=dashboard]");
-    private final ElementsCollection cards = $$(".list__item div");
+
 
     public DashboardPage() {
         heading.shouldBe(visible);
     }
 
-    public int getCardBalance(DataHelper.CardInfo cardInfo) {
+//    public Dashboard() {
+//    }
+
+        public int getCardBalance(DataHelper.CardInfo cardInfo) {
         var text = cards.findBy(text(cardInfo.getCardNumber().substring(15))).getText();
         return extractBalance(text);
     }
+//    public int getFirstCardBalance() {
+//        val text = cards.first().text();
+//        return extractBalance(text);
+//    }
+//
+//    private int extractBalance(String text) {
+//        val start = text.indexOf(balanceStart);
+//        val finish = text.indexOf(balanceFinish);
+//        val value = text.substring(start + balanceStart.length(), finish);
+//        return Integer.parseInt(value);
+//    }
 
     public int getCardBalance(int index) {
         var text = cards.get(index).getText();
